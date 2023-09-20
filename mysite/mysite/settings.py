812a92b23ec8b9a7bc2afdd99bca6276baf7d7ca
rishabhmadhok <rishabhm05@icloud.com>
2023-09-20 18:56:15 +0100

    allowed hosts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z*rpq!jk*il_3cq+nhr$dqas*&d(9w1=28!i*@&_4l^ck4ba)4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'rishabhmadhok.pythonanywhere.com']
 LOGIN_URL = '/login'
 
 
